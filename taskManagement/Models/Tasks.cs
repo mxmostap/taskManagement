9c55092b1543387604214e1018d1f50685bd0d84
mxmostap <mxmostapwork@gmail.com>
2025-10-06 23:56:53 +0300

    Initial commit

---
+++ taskManagement/Models/Tasks.cs
@@ -0,0 +1,10 @@
+namespace taskManagement.Models;
+
+public class Tasks
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
